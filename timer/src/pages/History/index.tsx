094b830a79192acc2aae2ddb14b1e40c0a376e0a
vinicius <viniciusrfscabral@gmail.com>
2025-02-08 00:15:29 -0300

    status

--- timer/src/pages/History/index.tsx
+++ timer/src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 
 export function History() {
@@ -21,31 +21,41 @@ export function History() {
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor = "red">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor = "red">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor = "red">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor = "red">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor = "red">Concluido</Status>
+                            </td>
                         </tr>
                         
                     </tbody>
