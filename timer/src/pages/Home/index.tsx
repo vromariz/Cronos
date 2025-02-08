63a620483941abf456f20b9ebc5f1d896b3dba3c
vinicius <viniciusrfscabral@gmail.com>
2025-02-08 02:23:14 -0300

    ciclo ativo

--- timer/src/pages/Home/index.tsx
+++ timer/src/pages/Home/index.tsx
@@ -2,8 +2,15 @@ import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod'
+import { useState } from "react";
 
-import { CountdowContainer, FormContainer, HomeConteiner, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import { CountdowContainer, 
+    FormContainer, HomeConteiner, 
+    MinutesAmountInput, Separator, 
+    StartCountdownButton, TaskInput 
+}from "./styles";
+
+
 
 const newCycleFormValidationSchema =  zod.object({
     task: zod.string().min(1, 'informe a tarefa'),
@@ -15,7 +22,16 @@ const newCycleFormValidationSchema =  zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle{
+    id: string;
+    task: string;
+    minutesAmount: number;
+}
+
 export function Home() {
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
     //register = recebe o nome do input e retorna metodos de monitoraçao de valores dos input
     const { register, handleSubmit , watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
@@ -26,9 +42,23 @@ export function Home() {
     })
 
     function handleCreateNewCycle(data: NewCycleFormData){
-        console.log(data)
-        reset();
+        const id= String(new Date().getTime());
+
+        const newCycle : Cycle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+        }
+
+        setCycles((state) => [...state, newCycle])
+        setActiveCycleId(id)
+
+        reset()
     }
+
+    const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId)
+
+    console.log(activeCycle)
 
     const task = watch('task')
     const isSubmitDisabled = !task;
