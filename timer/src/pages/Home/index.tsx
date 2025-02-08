d4ef26898cface072f6c8ed209af7cc831a3b24e
vinicius <viniciusrfscabral@gmail.com>
2025-02-08 02:36:17 -0300

    countdown

--- timer/src/pages/Home/index.tsx
+++ timer/src/pages/Home/index.tsx
@@ -31,6 +31,7 @@ interface Cycle{
 export function Home() {
     const [cycles, setCycles] = useState<Cycle[]>([])
     const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
     //register = recebe o nome do input e retorna metodos de monitoraçao de valores dos input
     const { register, handleSubmit , watch, reset } = useForm<NewCycleFormData>({
@@ -58,7 +59,14 @@ export function Home() {
 
     const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId)
 
-    console.log(activeCycle)
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0 
+
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmount = currentSeconds % 60
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
 
     const task = watch('task')
     const isSubmitDisabled = !task;
@@ -98,11 +106,11 @@ export function Home() {
 
 
                 <CountdowContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdowContainer>
                 <StartCountdownButton disabled={isSubmitDisabled} type="submit">
                     <Play size={24}/>
